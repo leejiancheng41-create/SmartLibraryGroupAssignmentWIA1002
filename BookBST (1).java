af141b17a0fbc163f7509d280f7994720aa1c5fd
Mahdei-2025 <24234913@siswa.um.edu.my>
2026-05-17 18:38:19 +0800

    Add files via upload

---
+++ BookBST (1).java
@@ -0,0 +1,36 @@
+ // a BST class to store books 
+public class BookBST {
+
+    private Book root; // the root node of the BST
+
+    // private method to insert a book into the BST
+    private  Book insertBook(Book root, long isbn, String title, String author) {
+        if (root == null) {
+            root = new Book(isbn, title, author);
+            System.out.println("Book added: \"" + title + "\" by " + author + " (ISBN: " + isbn + ")");
+            return root;  
+        }
+        else if (isbn < root.getIsbn()) {
+            root.left = insertBook(root.left, isbn, title, author);
+        }
+        else if (isbn > root.getIsbn()) {
+            root.right = insertBook(root.right, isbn, title, author);
+        }
+        else {System.out.println("Error: Book with ISBN " + isbn + " already exists in the catalogue.");}
+        return root;
+    }
+
+     // public method to insert a book into the BST
+    public void insert(long isbn, String title, String author) {
+        this.root = insertBook(this.root, isbn, title, author);
+    }
+     // private skeleton method to search for a book in the BST (for Record finder)
+    private Book searchBook(long isbn){
+        return null;
+    }
+     // public skeleton method to search for a book in the BST (for Record finder)
+    public Book search(long isbn) {
+        return null;
+    }
+    }
+
